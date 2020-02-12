a7aea086b52628297561e3718b145d48aa6c7e9a
Sébastien Meynet <toxicsed@gmail.com>
2020-02-12 19:43:17 +0100

    Application

--- SOS/App.js
+++ SOS/App.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity,} from 'react-native';
+
+
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Je suis dieu</Text>
-    </View>
-  );
-}
 
+  return (
+    <View style={{ flex:1, backgroundColor: 'yellow' }}>
+      <View style={{ flex:1, alignItems: 'center', justifyContent: 'center',  height: 335, backgroundColor: 'white' }}>
+      <TouchableOpacity style={styles.customBtnBG} onPress={() => {}}>
+<Text style={styles.customBtnText}>Pour moi</Text>
+</TouchableOpacity>
+</View>
+      <View style={{ flex:1, alignItems: 'center', justifyContent: 'center',  height: 335, backgroundColor: 'black' }}><TouchableOpacity style={styles.customBtnBG2} onPress={() => {}}>
+<Text style={styles.customBtnText2}>Pour autrui</Text>
+</TouchableOpacity></View>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+/* Here style the text of your button */
+customBtnText: {
+fontSize: 80,
+fontWeight: "bold",
+color: "orange",
+},
+
+/* Here style the background of your button */
+customBtnBG: {
+backgroundColor: "black",
+paddingHorizontal: 40,
+paddingVertical: 5,
+borderRadius: 35
+},
+/* Here style the text of your button */
+customBtnText2: {
+fontSize: 69,
+fontWeight: "bold",
+color: "black",
+},
+
+/* Here style the background of your button */
+customBtnBG2: {
+backgroundColor: "orange",
+paddingHorizontal: 30,
+paddingVertical: 5,
+borderRadius: 35
+}
 });
